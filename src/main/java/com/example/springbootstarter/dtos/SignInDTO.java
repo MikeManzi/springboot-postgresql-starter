d35e5acf5fd44291449ca0ddee79eb310f428532
MikeManzi <manzimike378@gmail.com>
2022-01-25 22:06:48 +0200

    (chore): sign in dto

---
+++ src/main/java/com/example/springbootstarter/dtos/SignInDTO.java
@@ -0,0 +1,15 @@
+package com.example.springbootstarter.dtos;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.validation.constraints.NotBlank;
+
+@Getter
+@Setter
+public class SignInDTO {
+    @NotBlank
+    private String login;
+    @NotBlank
+    private String password;
+}
